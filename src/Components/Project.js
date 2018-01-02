2760115783359e7da90d6a1d0dfbce11a7a13284
asnewman <ashznewman@gmail.com>
2018-01-01 21:00:21 -1000

    Made projects in md format

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,30 +1,18 @@
 import React, {Component} from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import github from '../img/github-logo.svg';
+
+const md = `
+# Kozo [<img alt="github logo" src="https://s3-us-west-2.amazonaws.com/www.ajkprojects.com/img/github-logo-big.png" width="25">](https://github.com/asnewman/Kozo)
+Kozo is an extended, commandline shell version of the stock broker app Robinhood. Additional features such as a trailing stoploss are made using [Robinhood's unofficial API](https://github.com/sanko/Robinhood). These features aim to trade stocks with less requirement for human interaction.
+`;
 
 class Project extends Component {
    render() {
       return (
          <div className="Project">
-            <div className="proj-title">
-               <h1>
-                  Kozo
-               </h1>
-               <div className="proj-link">
-                  <a href="https://github.com/asnewman/Kozo">
-                     <img alt="Github Logo" src={github} />
-                  </a>
-               </div>
-            </div>
-            <p>
-               Kozo is an extended, commandline shell version
-               of the stock broker app Robinhood. Additional
-               features such as a trailing stoploss are made
-               using <a href="https://github.com/sanko/Robinhood">
-               Robinhood's unofficial API</a>. 
-               These features aim to trade stocks with less 
-               requirement for human interaction.
-            </p>
+            <ReactMarkdown source={md} escapeHtml={false} />
          </div>
       );
    }
