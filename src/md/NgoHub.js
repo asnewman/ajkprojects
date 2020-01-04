d5ba3bb9df3dcada2523110f9d26c2b35f99c88d
Ashley Newman <ashznewman@gmail.com>
2020-01-04 01:00:21 -1000

    made youtube vids responsive

--- src/md/NgoHub.js
+++ src/md/NgoHub.js
@@ -4,7 +4,7 @@ This web app was created for the 2019 Zoo Hackathon in San Diego. Here was the m
 
 We were inspired by the efforts made by organizations that seek to end wildlife trafficking and poaching, who may wish to avoid conflicts or forge new collaborations in other groups working in the same area. We built a mobile-first web app that can be used on any device to allow organizations to learn when potentially conflicting initiatives could be taking place and contact those interested parties.
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/rlDpILvNaHA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe class="responsive-image" width="560" height="315" src="https://www.youtube.com/embed/rlDpILvNaHA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 `;
 
 export default content;
--- src/md/Pickmo.js
+++ src/md/Pickmo.js
@@ -2,7 +2,7 @@ const content = `
 # Pickmo (2017-2018)
 A semi-automated teleoperated strawberry picking system for the California Strawberry Commission. This was done in a group as a senior capstone project.
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/UEhTjCbdzqk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe class="responsive-image" width="560" height="315" src="https://www.youtube.com/embed/UEhTjCbdzqk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 `;
 
 export default content;
