29387cb8623a380c151eb39e0ff6316434537def
asnewman <ashznewman@gmail.com>
2020-06-01 00:12:58 -0700

    Added inner flux post

--- src/md/Blog.js
+++ src/md/Blog.js
@@ -1,5 +1,6 @@
 const content = `
 All my blog posts are on write.as, a privacy oriented minimal blogging website. Please see the links below to read them:
+## [Inner Flux](https://write.as/ashleynewman/in-the-past-few-weeks-i-have-been-thinking-a-lot-about-how-my-mentality)
 ## [Why Edward Snowden Believes a 24/7 Digital Census is Dangerous](https://write.as/ashleynewman/why-edward-snowden-believes-a-24-7-digital-census-is-dangerous)
 `;
 
