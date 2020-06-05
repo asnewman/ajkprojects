e481ec696f771bd70dfce9ef0e86d7ffc4f17127
asnewman <ashznewman@gmail.com>
2020-06-05 16:08:38 -0700

    Update Blog.js

--- src/md/Blog.js
+++ src/md/Blog.js
@@ -2,6 +2,9 @@ const content = `
 All my blog posts are on write.as, a privacy oriented minimal blogging website. Please see the links below to read them:
 ## [Inner Flux](https://write.as/ashleynewman/in-the-past-few-weeks-i-have-been-thinking-a-lot-about-how-my-mentality)
 ## [Why Edward Snowden Believes a 24/7 Digital Census is Dangerous](https://write.as/ashleynewman/why-edward-snowden-believes-a-24-7-digital-census-is-dangerous)
+
+Below are blog post that I have not written, but take great inspiration from:
+## [The Information Pathology](https://jjbeshara.com/2018/11/20/the-information-pathology-2/)
 `;
 
 export default content;
