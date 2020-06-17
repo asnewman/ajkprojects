169f5fb77cc956c67c00899de8a61f3131a8d9dc
asnewman <ashznewman@gmail.com>
2020-06-17 16:13:30 -0700

    Added fooodee

---
+++ src/md/Fooodee.js
@@ -0,0 +1,8 @@
+const content = `
+# Fooodee (present) [<img src="https://ajkprojects-assets.s3-us-west-2.amazonaws.com/github-logo.svg" alt="github logo" height="25" />](https://github.com/asnewman/fooodee)
+<img src="https://ajkprojects-assets.s3-us-west-2.amazonaws.com/fooodee.png" alt="Fooodee image"  class="responsive-image"/>
+
+A simple Svelte built website used to help oven-cookers quickly look up recommended temperature and time settings for their oven depending on what they are cooking. The idea was created from frusteration of recipe websites having obstructive and intrusive ads along with extra, unnecessary content.
+`;
+
+export default content;
