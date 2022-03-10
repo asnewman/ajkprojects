c1ff2ea75e9c1dca54799b1efc2dcfa9719ea114
asnewman <ashznewman@gmail.com>
2022-03-09 19:57:40 -0800

    Update Blog.js

--- src/md/Blog.js
+++ src/md/Blog.js
@@ -1,5 +1,6 @@
 const content = `
 All my blog posts are on write.as, a privacy oriented minimal blogging website. Please see the links below to read them:
+## [Be More Like SpongeBob](https://write.as/ashleynewman/be-more-like-spongebob)
 ## [McDonald's For a Week](https://write.as/ashleynewman/mcdonalds-for-a-week)
 ## [Inner Flux](https://write.as/ashleynewman/in-the-past-few-weeks-i-have-been-thinking-a-lot-about-how-my-mentality)
 ## [Why Edward Snowden Believes a 24/7 Digital Census is Dangerous](https://write.as/ashleynewman/why-edward-snowden-believes-a-24-7-digital-census-is-dangerous)
