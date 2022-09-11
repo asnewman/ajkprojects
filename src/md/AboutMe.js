c99865571e6ad72f226609553573aaad60f27af5
asnewman <ashznewman@gmail.com>
2022-09-11 10:59:43 -0700

    Update AboutMe.js

--- src/md/AboutMe.js
+++ src/md/AboutMe.js
@@ -1,5 +1,5 @@
 const content = `
-**Hello.** My name is Ashley Newman and I am software engineer from Mountain View, California. I was born in Tokyo, Japan and grew up in Hawaii. I went to California Polytechnic State University in San Luis Obispo (Cal Poly for short) and receive a degree in software engineering. Currently, I am a full-stack engineer at Ditto and also work on two side-projects called Kozukai and Speeched.
+**Hello.** My name is Ashley Newman and I am software engineer from Mountain View, California. I was born in Tokyo, Japan and grew up in Hawaii. I went to California Polytechnic State University in San Luis Obispo (Cal Poly for short) and receive a degree in software engineering. Currently, I am a full-stack engineer at Ditto and also work on two side-projects called Kozukai and nomodoom.
 
 My main interests in the software engineering field are the following:
 - Privacy
